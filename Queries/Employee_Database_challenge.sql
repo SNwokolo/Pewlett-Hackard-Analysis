7e8c5c9f482a0b36a78ba30292c788ca84ebffcd
SNwokolo <sandran812@gmail.com>
2023-01-02 04:30:47 -0500

    updating readme file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,9 +29,24 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
 INTO mentorship_eligibility
 FROM employees as e
 INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
+ON (e.emp_no = de.emp_no
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE de.to_date = ('9999-01-01')
 	AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 ORDER BY emp_no ASC;
+
+SELECT COUNT(me.emp_no), me.title
+INTO ment_titles
+FROM mentorship_eligibility as me
+GROUP BY me.title
+ORDER BY COUNT(me.title) DESC;
+	
+SELECT DISTINCT ON (me.emp_no) me.emp_no,
+me.first_name,
+me.last_name,
+me.from_date
+--INTO unique_titles
+FROM mentorship_eligibility as me
+WHERE to_date = ('9999-01-01')
+ORDER BY me.emp_no, me.from_date DESC;
